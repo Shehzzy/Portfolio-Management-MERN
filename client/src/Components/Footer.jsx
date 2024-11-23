f95d2ddd593bc7736ffa72f2522eed1fc69286c8
Shehzonia <aptechsheh@gmail.com>
2024-11-23 21:17:59 +0500

    Clean up alot of code. Make the rest of the pages in 1 day. Forms,tables etc and whatnot

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 function Footer() {
   return (
     <>
-    <footer>
-        <p>footer</p>
+    <footer className='d-flex justify-content-center align-items-center text-white p-5'>
+    <p>Trackfolio - Portfolio Management Tool</p>
+    <p className='mx-5'> Developed By Shehzonia fatima &lt;3333</p>
+    <p> Ok next is what</p>
+    
+
     </footer>
     </>
   )
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import Sidebar from '../Components/Sidebar'
 import '../assets/style.css';
 import FlashOn from '../assets/flash.png';
 import FlashOff from '../assets/flash_off.png';
+import User from '../assets/user.png';
 import Work from '../assets/work.png';
 import Footer from '../Components/Footer';
 import ProjectCards from '../Components/ProjectCards';
@@ -26,6 +27,26 @@ function Home() {
               </div>
             </div>
           </div>
+
+          <div className='row mt-5'>
+            <div className='col-lg-12 col-md-12 col-sm-12'>
+              <div className='card profile-card-2 d-flex justify-content-center align-items-center'>
+                <div className='card-logo d-flex justify-content-center align-items-center'>
+                  <img src={User} />
+                </div>
+                <h3 className='homepage-h3 mt-2'>Aban Ali</h3>
+                <ul>
+                  <li>Laravel Developer |</li>
+                  <li>Flutter Developer |</li>
+                  <li>Full Stack Developer</li>
+                 </ul>
+                 <p>Student - Aptech Metro Stargate</p>
+                 <p>Karachi, Pakistan</p>
+                 <p className="text-capitalize">A project for managing my portfolio projects.</p>
+              </div>
+            </div>
+          </div>
+
           <div className="row mt-3">
             <ProjectCards imgSrc={FlashOn} projectHeading={"Active Projects"} projectNumbers={0} />
             <ProjectCards imgSrc={FlashOff} projectHeading={"In-Active Projects"} projectNumbers={0} />
